63d989dcc4052c6a148f9d4b9a64116b814a8851
oAndin <00andherson@gmail.com>
2023-06-24 21:14:04 -0300

    Popup button feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+
+popupBtn.onclick=("openPopup", function() {
+    console.log("clickou");
+    popup.style.visibility = 'visible';
+});
+    
+myBtn.onclick=("closePopUp", function closePopup() {
+    popup.style.visibility = 'hidden';
+});
