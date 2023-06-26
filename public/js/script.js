59149cdd6eedcd5cab12fdb42a69a03011468cb3
oAndin <00andherson@gmail.com>
2023-06-26 19:54:48 -0300

    Finally made it

--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,21 @@ console.log("Hello World!");
 
 popupBtn.onclick=("openPopup", function() {
     console.log("clickou");
-    if (popup.style.visibility = "hidden") {
+    if ((popup.style.visibility) === (popup.style.visibility = "hidden")) {
         popup.style.visibility = "visible"
-    return console.log("Não");
+
     }
-    if (popup.style.visibility = "visible") {
-        popup.style.visibility = "hidden"
-    return console.log("Sim")
+    else {
+        ((popup.style.visibility) === (popup.style.visibility = "visible"))
+            popup.style.visibility = "hidden";
+        }
     }
+    // if ((popup.style.visibility) == (popup.style.visibility = "visible")){
+    //     popup.style.visibility = "hidden"
+    // return console.log("Sim")
+    // }
     // () ? :
-});
+);
     
 myBtn.onclick=("closePopUp", function closePopup() {
     popup.style.visibility = 'hidden';
