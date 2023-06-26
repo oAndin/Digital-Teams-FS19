bb2ac1c98f7a5ae5c73950571283a858cca9aa26
oAndin <00andherson@gmail.com>
2023-06-25 21:26:46 -0300

    Isso é uma bela pergunta para o gleidson

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,15 @@ console.log("Hello World!");
 
 popupBtn.onclick=("openPopup", function() {
     console.log("clickou");
-    popup.style.visibility = 'visible';
+    if (popup.style.visibility = "hidden") {
+        popup.style.visibility = "visible"
+    return console.log("Não");
+    }
+    if (popup.style.visibility = "visible") {
+        popup.style.visibility = "hidden"
+    return console.log("Sim")
+    }
+    // () ? :
 });
     
 myBtn.onclick=("closePopUp", function closePopup() {
