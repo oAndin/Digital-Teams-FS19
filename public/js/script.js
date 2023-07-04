f50f5bf704176282bfffbf35c6894d58789a8def
oAndin <00andherson@gmail.com>
2023-07-04 13:03:49 -0300

    Então é natal

--- public/js/script.js
+++ public/js/script.js
@@ -15,18 +15,28 @@ btnClose.onclick = function () {
 }
 
 
-let equipes = []
+let equipes = JSON.parse(localStorage.getItem('equipes')) || [];
 
 function listarEquipe() {
     listaDeEquipe.innerHTML = '';
-    for (let i = 0; i < equipes.length; i++) {
-        listaDeEquipe.innerHTML += `<li>
-        <h4>${equipes[i].nome}</h4>
-            <div class="infos">
-                <h2>${equipes[i].participantes} <span>/ ${equipes[i].qtdMax}</span></h2>
-                <button>adicionar<box-icon name='plus'></box-icon></button>
-            </div>
-        </li>`;
+    if (equipes.length > 0) {
+        for (let i = 0; i < equipes.length; i++) {
+            listaDeEquipe.innerHTML += 
+            `<li>
+            <h4>${equipes[i].nome}</h4>
+                <div class="infos">
+                    <h2>${equipes[i].participantes.length} <span>/ ${equipes[i].qtdMax}</span></h2>
+                    <div class="acoes">
+                    <button>adicionar</button>
+                    <button onClick='deletarEquipe(${i})'><box-icon name='trash'></box-icon></button>
+                    </div>
+                </div>
+            </li>`;
+        }
+    }
+    else {
+        listaDeEquipe.innerHTML = 
+        `<li class="sem-equipes">Crie sua primeira equipe!</li>`
     }
 }
 
@@ -39,9 +49,40 @@ function criarEquipe() {
         {
             nome: equipeNome.value,
             qtdMax: equipeQtn.value,
-            participantes:0,
+            participantes:[],
         }
         )
+        if(!localStorage.getItem('equipes')){
+            localStorage.setItem('equipes', JSON.stringify(equipes));
+        }else{
+            let equipesSalvas = JSON.parse(localStorage.getItem('equipes'));
+            equipesSalvas.push(
+                {
+                    nome: equipeNome.value,
+                    qtdMax:equipeQtn.value,
+                    participantes: []
+                }
+            );
+            localStorage.setItem('equipes', JSON.stringify(equipesSalvas));
+        }
+        formCriar.reset();
+        overlay.classList.remove('active');
+        modal.classList.remove('active');
         listarEquipe();
-}
+};
 
+function deletarEquipe(posicao) {
+    let equipesRestantes = [];
+    let equipesSalvas = JSON.parse(localStorage.getItem('equipes'));
+    for(let i = 0; i < equipes.length; i++) {
+        if( i != posicao ){
+        equipesRestantes.push(equipes[i]);
+    }
+    }
+    equipes = [];
+    equipes = equipesRestantes;
+    equipesSalvas = [];
+    equipes = equipesRestantes;
+    localStorage.setItem('equipes', JSON.stringify(equipesSalvas));
+    listarEquipe();
+};
